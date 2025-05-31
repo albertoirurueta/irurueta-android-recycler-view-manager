c29e052706ff78ddf6fa29615d5e4583b758a255
Alberto Irurueta Carro <albertoicar@ext.inditex.com>
2025-05-31 14:02:48 +0200

    Updated versions, removed unnecessary dependencies and fixed code coverage for unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.dokka)
-    alias(libs.plugins.jacoco)
     alias(libs.plugins.sonarqube)
 }
 
@@ -38,10 +37,6 @@ android {
     }
 
     buildTypes {
-        debug {
-            enableUnitTestCoverage = true
-            enableAndroidTestCoverage = true
-        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -61,10 +56,6 @@ android {
         compose = true
         buildConfig = true
     }
-}
-
-jacoco {
-    toolVersion = "0.8.12"
 }
 
 sonar {
@@ -102,22 +93,11 @@ sonar {
 
 dependencies {
     implementation(project(":lib"))
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.hermes)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.dokka)
-    alias(libs.plugins.jacoco)
     alias(libs.plugins.sonarqube)
 }
 
@@ -58,38 +57,6 @@ android {
     }
 }
 
-jacoco {
-    toolVersion = "0.8.12"
-}
-
-tasks.withType<Test>() {
-    configure<JacocoTaskExtension> {
-        isIncludeNoLocationClasses = true
-        excludes = listOf("jdk.internal.*")
-    }
-}
-
-configurations.all {
-    resolutionStrategy {
-        eachDependency {
-            val sysProps = System.getProperties()
-            if (sysProps["idea.platform.prefix"] != null) {
-                // Built from AndroidStudio
-                // When running from Android Studio, there is no need to generate jacoco report
-                // and nothing is changed so that debug variant of app can still run
-            } else {
-                // Built from command line
-                // Enabling this allows correct jacoco report generation for instrumentation tests
-                // in CI server, however prevents execution of debug variant of demo app in Android
-                // Studio
-                if ("org.jacoco" == this.requested.group) {
-                    this.useVersion("0.8.12")
-                }
-            }
-        }
-    }
-}
-
 sonar {
     properties {
         property("sonar.scanner.skipJreProvisioning", true)
@@ -98,28 +65,28 @@ sonar {
         property("sonar.organization", "albertoirurueta-github")
         property("sonar.host.url", "https://sonarcloud.io")
 
-        //property("sonar.tests", listOf("src/test/java", "src/androidTest/java"))
-        //property("sonar.test.inclusions",
-        //    listOf("**/*Test*/**", "src/androidTest/**", "src/test/**"))
-        //property("sonar.test.exclusions",
-        //    listOf("**/*Test*/**", "src/androidTest/**", "src/test/**"))
-        //property("sonar.sourceEncoding", "UTF-8")
-        //property("sonar.sources", "src/main/java")
-        //property("sonar.exclusions", "**/*Test*/**,*.json,'**/*test*/**,**/.gradle/**,**/R.class")
+        property("sonar.tests", listOf("src/test/java", "src/androidTest/java"))
+        property("sonar.test.inclusions",
+            listOf("**/*Test*/**", "src/androidTest/**", "src/test/**"))
+        property("sonar.test.exclusions",
+            listOf("**/*Test*/**", "src/androidTest/**", "src/test/**"))
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.sources", "src/main/java")
+        property("sonar.exclusions", "**/*Test*/**,*.json,'**/*test*/**,**/.gradle/**,**/R.class")
 
-        //val libraries = project.android.sdkDirectory.path + "/platforms/android-36/android.jar"
-        //property("sonar.libraries", libraries)
-        //property("sonar.java.libraries", libraries)
-        //property("sonar.java.test.libraries", libraries)
-        //property("sonar.binaries", "build/intermediates/javac/debug/classes,build/tmp/kotlin-classes/debug")
-        //property("sonar.java.binaries", "build/intermediates/javac/debug/classes,build/tmp/kotlin-classes/debug")
+        val libraries = project.android.sdkDirectory.path + "/platforms/android-36/android.jar"
+        property("sonar.libraries", libraries)
+        property("sonar.java.libraries", libraries)
+        property("sonar.java.test.libraries", libraries)
+        property("sonar.binaries", "build/intermediates/javac/debug/classes,build/tmp/kotlin-classes/debug")
+        property("sonar.java.binaries", "build/intermediates/javac/debug/classes,build/tmp/kotlin-classes/debug")
 
-        //property("sonar.coverage.jacoco.xmlReportPaths",
-        //    listOf("${project.layout.buildDirectory}/reports/coverage/androidTest/debug/report.xml",
-        //        "${project.layout.buildDirectory}/reports/coverage/test/report.xml"))
-        //property("sonar.java.coveragePlugin", "jacoco")
-        //property("sonar.junit.reportsPath", "build/test-results/testDebugUnitTest, build/outputs/androidTest-results/connected")
-        //property("sonar.android.lint.report", "build/reports/lint-results-debug.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths",
+            listOf("${project.layout.buildDirectory}/reports/coverage/androidTest/debug/report.xml",
+                "${project.layout.buildDirectory}/reports/coverage/test/report.xml"))
+        property("sonar.java.coveragePlugin", "jacoco")
+        property("sonar.junit.reportsPath", "build/test-results/testDebugUnitTest, build/outputs/androidTest-results/connected")
+        property("sonar.android.lint.report", "build/reports/lint-results-debug.xml")
     }
 }
 
@@ -133,20 +100,14 @@ ext {
     set("PUBLISH_ARTIFACT_ID", "irurueta-android-recycler-view")
 }
 
-apply(from = "${rootProject.projectDir}/scripts/publish-module.gradle")
-
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.material)
     api(libs.hermes)
     testImplementation(libs.junit)
     testImplementation(libs.mockk.android)
-    testImplementation(libs.mockk.agent)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.test.core.ktx)
     androidTestImplementation(libs.androidx.test.ext.junit.ktx)
     androidTestImplementation(libs.mockk.android)
-    androidTestImplementation(libs.mockk.agent)
 }
