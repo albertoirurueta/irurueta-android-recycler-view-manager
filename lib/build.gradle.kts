ee2b64cdc6aa30fea296b05a85fa08dca8590831
Alberto Irurueta Carro <albertoicar@ext.inditex.com>
2025-05-31 15:46:56 +0200

    Updated sonar configuration

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -82,8 +82,7 @@ sonar {
         property("sonar.java.binaries", "build/intermediates/javac/debug/classes,build/tmp/kotlin-classes/debug")
 
         property("sonar.coverage.jacoco.xmlReportPaths",
-            listOf("${project.layout.buildDirectory}/reports/coverage/androidTest/debug/report.xml",
-                "${project.layout.buildDirectory}/reports/coverage/test/report.xml"))
+            listOf("${project.layout.buildDirectory}/reports/coverage/test/report.xml"))
         property("sonar.java.coveragePlugin", "jacoco")
         property("sonar.junit.reportsPath", "build/test-results/testDebugUnitTest, build/outputs/androidTest-results/connected")
         property("sonar.android.lint.report", "build/reports/lint-results-debug.xml")
