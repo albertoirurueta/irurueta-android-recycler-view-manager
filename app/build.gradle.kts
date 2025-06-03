e03db2842fbfb5b07416fc87d60770bf660ce932
Alberto Irurueta Carro <albertoicar@ext.inditex.com>
2025-06-03 16:51:26 +0200

    Added alias for publish plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.irurueta.android.recyclerviewmanager.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
